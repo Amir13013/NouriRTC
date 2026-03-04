a2639df9477e4b4551e168a61b530c90a848736b
Augustin <augustin.viemont1@outlook.fr>
2026-03-04 13:09:12 +0100

    updated DataBase.sql

--- DataBase/DataBase.sql
+++ DataBase/DataBase.sql
@@ -18,11 +18,15 @@ CREATE TABLE users_servers (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL,
     server_id BIGINT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'member',
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (server_id) REFERENCES servers(id) ON DELETE CASCADE,
 
-    UNIQUE (user_id, server_id)
+    CONSTRAINT unique_user_server UNIQUE (user_id, server_id),
+
+    CONSTRAINT check_role
+    CHECK (role IN ('owner', 'admin', 'member'))
 );
 
 CREATE TABLE channels (
